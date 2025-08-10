fc416b65e6ff0789db3bb7fd61a57cabe767f970
yuvraj-rathod-1202 <rathodyuvraj12022007@gmail.com>
2025-08-10 11:55:59 +0530

    add

--- src/context/AuthContext.jsx
+++
@@ -1,6 +0,0 @@
-/*
-
-    Auth Context for managing user authentication state in the TEDx Orientation App.
-    This context provides methods to log in, log out, and check if a user is authenticated
-
-*/
